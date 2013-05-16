d181d251f89cdf5e10c4918193d0ce85983f2b7f
Dustin Lim <lim.dustin@gmail.com>
2013-05-16 16:28:29 +0200

    Real-time charts geimplementeerd

--- CLESMonitor/CLESMonitor/Controller/ViewController.cs
+++ CLESMonitor/CLESMonitor/Controller/ViewController.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using CLESMonitor.Model;
 using CLESMonitor.View;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace CLESMonitor.Controller
 {
     class ViewController
     {
+        private const double TIME_WINDOW = 0.5; //in minuten
+        private const int LOOP_SLEEP_INTERVAL = 1000; //in milliseconden
+
         private CLModel clModel;
         private ESModel esModel;
         private CLESMonitorViewForm _view;
+        private Thread updateChartDataThread;
+        private Random random = new Random();
+
+        public delegate void UpdateChartDataDelegate();
+        public UpdateChartDataDelegate updateCLChartDataDelegate;
+        public UpdateChartDataDelegate updateESChartDataDelegate;
+
+        // Outlets
+        Chart CLChart;
+        Chart ESChart;
+        TextBox clTextBox;
+        TextBox esTextBox;
 
         public CLESMonitorViewForm View
         {
@@ -24,11 +44,118 @@ namespace CLESMonitor.Controller
 
         public ViewController()
         {
-            _view = new CLESMonitorViewForm();    
-        }   
+            _view = new CLESMonitorViewForm();
 
+            // Stel outlets in
+            CLChart = this.View.CLChart;
+            ESChart = this.View.ESChart;
+            clTextBox = this.View.clTextBox;
+            esTextBox = this.View.esTextBox;
 
+            // Creëer een thread voor de real-time grafiek - nog niet starten
+            ThreadStart updateChartDataThreadStart = new ThreadStart(UpdateChartDataLoop);
+            updateChartDataThread = new Thread(updateChartDataThreadStart);
 
+            // Wijs delegates toe
+            updateCLChartDataDelegate += new UpdateChartDataDelegate(UpdateCLChartData);
+            updateESChartDataDelegate += new UpdateChartDataDelegate(UpdateESChartData);
 
+            startTrending_Click(null, null);
+        }
+
+        private void UpdateChartDataLoop()
+        {
+            while (true)
+            {
+                CLChart.Invoke(updateCLChartDataDelegate);
+                ESChart.Invoke(updateESChartDataDelegate);
+
+                Thread.Sleep(LOOP_SLEEP_INTERVAL);
+            }
+        }
+
+        public void UpdateCLChartData()
+        {
+            DateTime timeStamp = DateTime.Now;
+            Series series = CLChart.Series["Series1"];
+
+            double newDataValue = AddNewPoint(timeStamp, series);
+
+            // Update chart
+            CLChart.ChartAreas[0].AxisX.Minimum = series.Points[0].XValue;
+            CLChart.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(series.Points[0].XValue).AddMinutes(TIME_WINDOW).ToOADate();
+            CLChart.Invalidate(); //redraw
+
+            // Update TextBox
+            clTextBox.Text = newDataValue.ToString();
+        }
+
+        public void UpdateESChartData()
+        {
+            DateTime timeStamp = DateTime.Now;
+            Series series = ESChart.Series["Series1"];
+
+            double newDataValue = AddNewPoint(timeStamp, series);
+
+            // Update chart
+            ESChart.ChartAreas[0].AxisX.Minimum = series.Points[0].XValue;
+            ESChart.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(series.Points[0].XValue).AddMinutes(TIME_WINDOW).ToOADate();
+            ESChart.Invalidate(); //redraw
+
+            // Update TextBox
+            esTextBox.Text = newDataValue.ToString();
+        }
+
+        /// The AddNewPoint function is called for each series in the chart when
+        /// new points need to be added.  The new point will be placed at specified
+        /// X axis (Date/Time) position with a random Y value
+        public double AddNewPoint(DateTime timeStamp, System.Windows.Forms.DataVisualization.Charting.Series ptSeries)
+        {
+            // Add new data point to its series.
+            double newPoint = random.Next(5, 15);
+            ptSeries.Points.AddXY(timeStamp.ToOADate(), newPoint);
+
+            // Remove all points from the source series older than the time window
+            double removeBefore = timeStamp.AddSeconds((double)(60) * (-TIME_WINDOW)).ToOADate();
+            while (ptSeries.Points[0].XValue < removeBefore)
+            {
+                ptSeries.Points.RemoveAt(0);
+            }
+
+            return newPoint;
+        }
+
+        private void startTrending_Click(object sender, System.EventArgs e)
+        {
+            // Predefine the viewing area of the chart
+            DateTime minValue = DateTime.Now;
+            DateTime maxValue = minValue.AddSeconds(TIME_WINDOW*60);
+
+            CLChart.ChartAreas[0].AxisX.Minimum = minValue.ToOADate();
+            CLChart.ChartAreas[0].AxisX.Maximum = maxValue.ToOADate();
+            ESChart.ChartAreas[0].AxisX.Minimum = minValue.ToOADate();
+            ESChart.ChartAreas[0].AxisX.Maximum = maxValue.ToOADate();
+
+            // Setup van chart1
+            CLChart.Series.Clear();
+            Series newSeries = new Series("Series1");
+            newSeries.ChartType = SeriesChartType.Line;
+            newSeries.BorderWidth = 2;
+            newSeries.Color = Color.OrangeRed;
+            newSeries.XValueType = ChartValueType.DateTime;
+            CLChart.Series.Add(newSeries);
+
+            // Setup van chart2
+            ESChart.Series.Clear();
+            Series newSeries2 = new Series("Series1");
+            newSeries2.ChartType = SeriesChartType.Line;
+            newSeries2.BorderWidth = 2;
+            newSeries2.Color = Color.Blue;
+            newSeries2.XValueType = ChartValueType.DateTime;
+            ESChart.Series.Add(newSeries2);
+
+            // Start de thread
+            updateChartDataThread.Start();
+        }
     }
 }
--- CLESMonitor/CLESMonitor/View/CLESMonitorViewForm.Designer.cs
+++ CLESMonitor/CLESMonitor/View/CLESMonitorViewForm.Designer.cs
@@ -29,22 +29,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.CLChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.ESChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.startButton = new System.Windows.Forms.Button();
             this.stopButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.CLtextBox = new System.Windows.Forms.TextBox();
-            this.EStextBox = new System.Windows.Forms.TextBox();
+            this.clTextBox = new System.Windows.Forms.TextBox();
+            this.esTextBox = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.CLChart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ESChart)).BeginInit();
@@ -54,46 +54,46 @@
             // 
             this.CLChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            chartArea1.Name = "ChartArea1";
-            this.CLChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.CLChart.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.CLChart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.CLChart.Legends.Add(legend3);
             this.CLChart.Location = new System.Drawing.Point(12, 12);
             this.CLChart.Name = "CLChart";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.CLChart.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.CLChart.Series.Add(series3);
             this.CLChart.Size = new System.Drawing.Size(456, 369);
             this.CLChart.TabIndex = 0;
             this.CLChart.Text = "chart1";
-            title1.Name = "CL-Waarden";
-            title1.Text = "CL-Waarden";
-            this.CLChart.Titles.Add(title1);
+            title3.Name = "CL-Waarden";
+            title3.Text = "CL-Waarden";
+            this.CLChart.Titles.Add(title3);
             this.CLChart.Click += new System.EventHandler(this.CLChart_Click);
             // 
             // ESChart
             // 
             this.ESChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea2.Name = "ChartArea1";
-            this.ESChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.ESChart.Legends.Add(legend2);
+            chartArea4.Name = "ChartArea1";
+            this.ESChart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.ESChart.Legends.Add(legend4);
             this.ESChart.Location = new System.Drawing.Point(520, 12);
             this.ESChart.Name = "ESChart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.ESChart.Series.Add(series2);
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.ESChart.Series.Add(series4);
             this.ESChart.Size = new System.Drawing.Size(456, 369);
             this.ESChart.TabIndex = 1;
             this.ESChart.Text = "chart2";
-            title2.Name = "ES-waarden";
-            title2.Text = "ES-Waarden";
-            this.ESChart.Titles.Add(title2);
+            title4.Name = "ES-waarden";
+            title4.Text = "ES-Waarden";
+            this.ESChart.Titles.Add(title4);
             // 
             // startButton
             // 
@@ -133,19 +133,19 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "ES-waarde";
             // 
-            // CLtextBox
+            // clTextBox
             // 
-            this.CLtextBox.Location = new System.Drawing.Point(86, 400);
-            this.CLtextBox.Name = "CLtextBox";
-            this.CLtextBox.Size = new System.Drawing.Size(50, 20);
-            this.CLtextBox.TabIndex = 7;
+            this.clTextBox.Location = new System.Drawing.Point(86, 400);
+            this.clTextBox.Name = "clTextBox";
+            this.clTextBox.Size = new System.Drawing.Size(50, 20);
+            this.clTextBox.TabIndex = 7;
             // 
-            // EStextBox
+            // esTextBox
             // 
-            this.EStextBox.Location = new System.Drawing.Point(594, 403);
-            this.EStextBox.Name = "EStextBox";
-            this.EStextBox.Size = new System.Drawing.Size(50, 20);
-            this.EStextBox.TabIndex = 8;
+            this.esTextBox.Location = new System.Drawing.Point(594, 403);
+            this.esTextBox.Name = "esTextBox";
+            this.esTextBox.Size = new System.Drawing.Size(50, 20);
+            this.esTextBox.TabIndex = 8;
             // 
             // richTextBox1
             // 
@@ -155,22 +155,22 @@
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
             // 
-            // Form1
+            // CLESMonitorViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(988, 699);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.EStextBox);
-            this.Controls.Add(this.CLtextBox);
+            this.Controls.Add(this.esTextBox);
+            this.Controls.Add(this.clTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.stopButton);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.ESChart);
             this.Controls.Add(this.CLChart);
-            this.Name = "Form1";
+            this.Name = "CLESMonitorViewForm";
             this.Text = "CLES-Monitor";
             ((System.ComponentModel.ISupportInitialize)(this.CLChart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ESChart)).EndInit();
@@ -181,15 +181,15 @@
 
         #endregion
 
-        protected System.Windows.Forms.DataVisualization.Charting.Chart CLChart;
-        protected System.Windows.Forms.DataVisualization.Charting.Chart ESChart;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button stopButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox CLtextBox;
-        private System.Windows.Forms.TextBox EStextBox;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart CLChart;
+        public System.Windows.Forms.DataVisualization.Charting.Chart ESChart;
+        public System.Windows.Forms.TextBox clTextBox;
+        public System.Windows.Forms.TextBox esTextBox;
 
     }
 }
