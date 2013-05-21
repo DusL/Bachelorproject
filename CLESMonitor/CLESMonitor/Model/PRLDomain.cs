21f234cf505174eaa48698642acb21ff30f21dfe
Veerle van der Tas <veerlevandertas@gmail.com>
2013-05-21 15:51:29 +0200

    Alle taken aangemaakt in PRLDomain + Voorbeeld tekstbestand toegevoegd voor inlezen gesimuleerde data.

--- CLESMonitor/CLESMonitor/Model/PRLDomain.cs
+++ CLESMonitor/CLESMonitor/Model/PRLDomain.cs
@@ -20,16 +20,109 @@ namespace CLESMonitor.Model
         {
             taskArray = new CTLTask[2];
 
-            CTLTask task = new CTLTask("ARI uitschakelen voor geselecteerde planregels");
+            CTLTask task = new CTLTask("ARI_UIT");
+            task.description = "ARI uitschakelen voor geselecteerde planregels";
             task.moValue = 0;
             task.lipValue = 0;
             task.domains =  new InformationDomain[]{InformationDomain.InformationDomainUnknown};
             taskArray[0] = task;
 
-            task = new CTLTask("ARI inschakelen voor geselecteerde planregels");
+            task = new CTLTask("ARI_IN");
+            task.description = "ARI inschakelen voor geselecteerde planregels";
             task.moValue = 0;
             task.lipValue = 0;
             task.domains = new InformationDomain[] {InformationDomain.InformationDomainUnknown};
+            taskArray[0] = task;
+
+            task = new CTLTask("VIND_TREIN");
+            task.description = "Vind planregel met specifiek treinnummer";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("SELECTEER_REGEL");
+            task.description = "Selecteer planregel";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("REGEL_IN_MUTATIESCHERM");
+            task.description = "Planregel in mutatiescherm plaatsen";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("MUTEER_REGEL");
+            task.description = "Planregel muteren";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("REGEL_TERUG");
+            task.description = "Planregel terug plaatsen";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("HAND_VERWERK_REGEL");
+            task.description = "planregel handmatig verwerken";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("VERWERK_VERT_REGELS");
+            task.description = "Vertraging verwerken voor geselecteerde planregels";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("KWIT_VERT_REGELS");
+            task.description = "Vertraging kwiteren voor geselecteerde planregels";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("VERWERK_VERT_TREIN");
+            task.description = "Vertraging verwerken voor trein";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("VERWERK_VERT_REGELS");
+            task.description = "Vertraging verwerken voor geselecteerde planregels";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("LASTGEVING");
+            task.description = "Lastgeving";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("HERROEP_SEIN");
+            task.description = "Sein herroepen";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+            taskArray[0] = task;
+
+            task = new CTLTask("COMMUNICATIE");
+            task.description = "Communicatie met andere processleiders en externe partijen";
+            task.moValue = 0;
+            task.lipValue = 0;
+            task.domains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
             taskArray[0] = task;
 
             return taskArray;
