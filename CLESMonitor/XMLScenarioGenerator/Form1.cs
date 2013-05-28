96a242d17fb7e6d4b48e32ae701871ec3297c5a7
Dustin Lim <lim.dustin@gmail.com>
2013-05-28 10:38:03 +0200

    Created subproject XMLScenarioGenerator

---
+++ CLESMonitor/XMLScenarioGenerator/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XMLScenarioGenerator
+{
+    public partial class Form1 : Form
+    {
+        private ViewController _controller;
+
+        public Form1(ViewController controller)
+        {
+            InitializeComponent();
+            _controller = controller;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _controller.generateXMLFile();
+        }
+    }
+}
---
+++ CLESMonitor/XMLScenarioGenerator/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XMLScenarioGenerator
+{
+    public class ViewController
+    {
+        private Form1 _view;
+        public Form1 view
+        {
+            get
+            {
+                return _view;
+            }
+        }
+
+        // Outlets
+        TextBox textBox;
+
+        public ViewController()
+        {
+            _view = new Form1(this);
+
+            textBox = _view.textBox1;
+        }
+
+        public void generateXMLFile()
+        {
+            Console.WriteLine(textBox.Text);
+
+            XDocument xmlDocument = new XDocument(
+                new XElement("Root",
+                    new XElement("Child", "content")
+                )
+            );
+            
+            xmlDocument.Save("testje.xml");
+        }
+    }
+}
