56956dc643c1751b2873e24b3d7969ff6379a6d3
Dustin Lim <lim.dustin@gmail.com>
2013-05-28 12:17:13 +0200

    Basisimplementatie gemaakt voor de XMLScenarioGenerator Generator ondersteund nog geen meerdere events

--- CLESMonitor/XMLScenarioGenerator/ViewController.cs
+++ CLESMonitor/XMLScenarioGenerator/ViewController.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
+using System.Xml;
 
 namespace XMLScenarioGenerator
 {
+    public struct Element
+    {
+        public ElementType type;
+        public string name;
+        public int startSecond;
+        public int duration;
+
+        public Element(ElementType _type, string _name, int _startSecond, int _duration)
+        {
+            type = _type;
+            name = _name;
+            startSecond = _startSecond;
+            duration = _duration;
+        }
+    }
+
+    public enum ElementType
+    {
+        Unknown,
+        Event,
+        Task
+    }
+
     public class ViewController
     {
+        List<Element> elements;
+        int scenarioLength;
+
         private Form1 _view;
         public Form1 view
         {
@@ -19,27 +45,82 @@ namespace XMLScenarioGenerator
             }
         }
 
-        // Outlets
-        TextBox textBox;
-
         public ViewController()
         {
+            elements = new List<Element>();
             _view = new Form1(this);
 
-            textBox = _view.textBox1;
+            // Add the (hardcoded) data to be generated here
+            scenarioLength = 35;
+            elements.Add(new Element(ElementType.Event, "EVENT_IDENTIFIER_1", 2, 30));
+            elements.Add(new Element(ElementType.Task, "TASK_IDENTIFIER_2", 15, 5));
+            elements.Add(new Element(ElementType.Task, "TASK_IDENTIFIER_3", 20, 0));
         }
 
+        /// <summary>
+        /// This methods is called when the button in the GUI is pressed
+        /// and will generate and save the XML file.
+        /// </summary>
         public void generateXMLFile()
         {
-            Console.WriteLine(textBox.Text);
+            XmlTextWriter writer = new XmlTextWriter("GeneratedScenario.xml", null);
+            writer.Formatting = Formatting.Indented;
 
-            XDocument xmlDocument = new XDocument(
-                new XElement("Root",
-                    new XElement("Child", "content")
-                )
-            );
-            
-            xmlDocument.Save("testje.xml");
+            writer.WriteStartElement("scenario");
+
+            // For each second in the generated scenario
+            for (int i=1; i <= scenarioLength; i++)
+            {
+                writer.WriteStartElement("second", null);
+                writer.WriteAttributeString("id", Convert.ToString(i));
+
+                foreach (Element element in elements)
+                {
+                    // Check whether an element starts now
+                    if (element.startSecond == i)
+                    {
+                        if (element.type == ElementType.Event) {
+                            writer.WriteStartElement("event", null);
+                        }
+                        else if (element.type == ElementType.Task) {
+                            writer.WriteStartElement("task", null);
+                        }
+                        writer.WriteStartElement("name", null);
+                        writer.WriteString(element.name);
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("action", null);
+                        writer.WriteString("started");
+                        writer.WriteEndElement();
+                        writer.WriteEndElement();
+                    }
+
+                    // Check whether an element ends now
+                    if (element.startSecond + element.duration == i)
+                    {
+                        if (element.type == ElementType.Event)
+                        {
+                            writer.WriteStartElement("event", null);
+                        }
+                        else if (element.type == ElementType.Task)
+                        {
+                            writer.WriteStartElement("task", null);
+                        }
+                        writer.WriteStartElement("name", null);
+                        writer.WriteString(element.name);
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("action", null);
+                        writer.WriteString("stopped");
+                        writer.WriteEndElement();
+                        writer.WriteEndElement();
+                    }
+                }
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
         }
     }
 }
