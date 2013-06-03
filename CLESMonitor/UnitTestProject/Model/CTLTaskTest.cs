27c71a15324624c0d531851604a81c14dd2392fd
Veerle van der Tas <veerlevandertas@gmail.com>
2013-06-03 10:55:59 +0200

    Added a UnitTest solution; created separate folders for model, view and controller. Added 3 example test classes.

---
+++ CLESMonitor/UnitTestProject/Model/CTLTaskTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTest.Model
+{
+    [TestClass]
+    public class CTLTaskTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
