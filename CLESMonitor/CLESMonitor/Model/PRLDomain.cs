a0bd3ad86d1d800dd5c673e75af3598f5894875e
Dustin Lim <lim.dustin@gmail.com>
2013-06-05 15:28:11 +0200

    Added comments to PRLDomain Created test suite for PRLDomain

--- CLESMonitor/CLESMonitor/Model/PRLDomain.cs
+++ CLESMonitor/CLESMonitor/Model/PRLDomain.cs
@@ -16,23 +16,31 @@ namespace CLESMonitor.Model
         InformationDomainExternalContact
     }
 
+    /// <summary>
+    /// This is a support class for CTLModel. It can generate the appropriate classes for
+    /// CTLModel from the classes it receives from XMLTaskParser. The class is domain-specific,
+    /// this implementation will generate data in the context of cognitive load for
+    /// railroad-operators.
+    /// </summary>
     public class PRLDomain
     {
+        /// <summary>
+        /// Generates a CTLEvent instance from a ParsedEvent instance
+        /// </summary>
+        /// <param name="parsedEvent">The ParsedEvent to generate from</param>
+        /// <returns>The generated CTLEvent</returns>
         public CTLEvent generateEvent(ParsedEvent parsedEvent)
         {
             CTLEvent ctlEvent = null;
 
-            if(parsedEvent.type != null && parsedEvent.identifier != null)
+            List<string> validTypes = new List<string>();
+            validTypes.Add("GESTRANDE_TREIN");
+            validTypes.Add("GESTOORDE_WISSEL");
+            validTypes.Add("VERTRAAGDE_TREIN");
+
+            if (parsedEvent != null && parsedEvent.type != null && parsedEvent.identifier != null)
             {
-                if (parsedEvent.type.Equals("GESTRANDE_TREIN"))
-                {
-                    ctlEvent = new CTLEvent(parsedEvent.identifier, parsedEvent.type);
-                }
-                else if (parsedEvent.type.Equals("GESTOORDE_WISSEL"))
-                {
-                    ctlEvent = new CTLEvent(parsedEvent.identifier, parsedEvent.type);
-                }
-                else if (parsedEvent.type.Equals("VERTRAAGDE_TREIN"))
+                if (validTypes.Contains(parsedEvent.type))
                 {
                     ctlEvent = new CTLEvent(parsedEvent.identifier, parsedEvent.type);
                 }
@@ -42,147 +50,117 @@ namespace CLESMonitor.Model
         }
 
         /// <summary>
-        /// Gets the task by means of its string identifier
+        /// Generates a CTLTask instance from a ParsedTask instance
         /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
+        /// <param name="parsedTask">The ParsedTask to generate from</param>
+        /// <returns>The generated CTLTask</returns>
         public CTLTask generateTask(ParsedTask parsedTask)
         {
-            CTLTask task = null;
-            if (parsedTask.type != null && parsedTask.identifier != null)
+            CTLTask ctlTask = null;
+
+            if (parsedTask != null && parsedTask.type != null && parsedTask.identifier != null)
             {
-
-
                 if (parsedTask.type.Equals("ARI_UIT"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "ARI_UIT");
-                    task.description = "ARI uitschakelen voor geselecteerde planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "ARI_UIT");
+                    ctlTask.description = "ARI uitschakelen voor geselecteerde planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("ARI_IN"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "ARI_IN");
-                    task.description = "ARI inschakelen voor geselecteerde planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "ARI_IN");
+                    ctlTask.description = "ARI inschakelen voor geselecteerde planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("VIND_TREIN"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "VIND_TREIN");
-                    task.description = "Vind planregel met specifiek treinnummer";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "VIND_TREIN");
+                    ctlTask.description = "Vind planregel met specifiek treinnummer";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("SELECTEER_REGEL"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "SELECTEER_REGEL");
-                    task.description = "Selecteer planregel";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "SELECTEER_REGEL");
+                    ctlTask.description = "Selecteer planregel";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("DESELECTEER_REGEL"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "DESELECTEER_REGEL");
-                    task.description = "De-selecteer planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "DESELECTEER_REGEL");
+                    ctlTask.description = "De-selecteer planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("REGEL_IN_MUTATIESCHERM"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "REGEL_IN_MUTATIESCHERM");
-                    task.description = "Planregel in mutatiescherm plaatsen";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "REGEL_IN_MUTATIESCHERM");
+                    ctlTask.description = "Planregel in mutatiescherm plaatsen";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("MUTEER_REGEL"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "MUTEER_REGEL");
-                    task.description = "Planregel muteren";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "MUTEER_REGEL");
+                    ctlTask.description = "Planregel muteren";
+                    ctlTask.moValue = 0;
+                    ctlTask.lipValue = 0;
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("REGEL_TERUG"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "REGEL_TERUG");
-                    task.description = "Planregel terug plaatsen";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "REGEL_TERUG");
+                    ctlTask.description = "Planregel terug plaatsen";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("HAND_VERWERK_REGEL"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "HAND_VERWERK_REGEL");
-                    task.description = "planregel handmatig verwerken";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "HAND_VERWERK_REGEL");
+                    ctlTask.description = "planregel handmatig verwerken";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("VERWERK_VERT_REGELS"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "VERWERK_VERT_REGELS");
-                    task.description = "Vertraging verwerken voor geselecteerde planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "VERWERK_VERT_REGELS");
+                    ctlTask.description = "Vertraging verwerken voor geselecteerde planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("KWIT_VERT_REGELS"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "KWIT_VERT_REGELS");
-                    task.description = "Vertraging kwiteren voor geselecteerde planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "KWIT_VERT_REGELS");
+                    ctlTask.description = "Vertraging kwiteren voor geselecteerde planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("VERWERK_VERT_TREIN"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "VERWERK_VERT_TREIN");
-                    task.description = "Vertraging verwerken voor trein";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "VERWERK_VERT_TREIN");
+                    ctlTask.description = "Vertraging verwerken voor trein";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("VERWERK_VERT_REGELS"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "VERWERK_VERT_REGELS");
-                    task.description = "Vertraging verwerken voor geselecteerde planregels";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "VERWERK_VERT_REGELS");
+                    ctlTask.description = "Vertraging verwerken voor geselecteerde planregels";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("LASTGEVING"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "LASTGEVING");
-                    task.description = "Lastgeving";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "LASTGEVING");
+                    ctlTask.description = "Lastgeving";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("HERROEP_SEIN"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "HERROEP_SEIN");
-                    task.description = "Sein herroepen";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "HERROEP_SEIN");
+                    ctlTask.description = "Sein herroepen";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
                 else if (parsedTask.type.Equals("COMMUNICATIE"))
                 {
-                    task = new CTLTask(parsedTask.identifier, "COMMUNICATIE");
-                    task.description = "Communicatie met andere processleiders en externe partijen";
-                    task.moValue = 0;
-                    task.lipValue = 0;
-                    task.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
+                    ctlTask = new CTLTask(parsedTask.identifier, "COMMUNICATIE");
+                    ctlTask.description = "Communicatie met andere processleiders en externe partijen";
+                    ctlTask.informationDomains = new InformationDomain[] { InformationDomain.InformationDomainUnknown };
                 }
             }
-            return task;
+
+            return ctlTask;
         }       
     }
 }
